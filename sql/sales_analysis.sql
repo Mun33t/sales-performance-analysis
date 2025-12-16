85e76fb844690ffa0966e9e6e750a24b5ab330eb
Mun33t <muneetpruthi@gmail.com>
2025-12-16 12:47:36 -0500

    Add initial data preview query

--- sql/sales_analysis.sql
+++ sql/sales_analysis.sql
@@ -2,3 +2,8 @@
 -- Author: Muneet Pruthi
 -- Purpose: Analyze sales data to identify top regions, products, and categories
 -- Database: PostgreSQL-style SQL
+
+-- Preview the dataset (first 10 rows)
+SELECT *
+FROM orders
+LIMIT 10;
